d124fe309a1b15d26d1b510ac16ea609aa7a8230
willyopera <willy4opera@gmail.com>
2024-03-14 15:47:35 -0400

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Basic annotations - to string.
+'''
+
+
+def to_str(n: float) -> str:
+    '''Converts the floating-point 'n' number to a string.
+    '''
+    return str(n)

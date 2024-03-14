d0ce9881dec7ef9d6d50ab80a8496e6a5eaec36f
willyopera <willy4opera@gmail.com>
2024-03-14 15:33:50 -0400

    Added the 1-concat.py TAsk

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''The Task 1's Concatenate module.
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''Function Concatenates two strings.
+    '''
+    return str1 + str2

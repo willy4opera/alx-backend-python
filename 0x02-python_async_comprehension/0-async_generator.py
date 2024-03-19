e5a2b2f75ede84f0e706105d55834ac64ada4788
willyopera <willy4opera@gmail.com>
2024-03-19 17:42:04 -0400

    Added the 200~0-async_generator.py

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+'''Async Generator
+'''
+import asyncio
+import random
+
+
+
+async def async_generator():
+    '''Here, we generated a sequence of 10 numbers.ls
+    '''
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10

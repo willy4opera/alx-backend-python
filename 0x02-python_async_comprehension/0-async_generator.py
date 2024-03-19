4867810c7685614df3db13d153222a162361ad63
willyopera <willy4opera@gmail.com>
2024-03-19 17:46:24 -0400

    Debugged the 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@
 '''
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     '''Here, we generated a sequence of 10 numbers.ls
     '''
     for _ in range(10):
